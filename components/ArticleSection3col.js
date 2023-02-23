ad6482375f10a6f650dbe427120c7f7428a71a22
martinbolsnes <martinbolsnes@icloud.com>
2023-02-23 14:58:34 +0100

    create article sections

---
+++ components/ArticleSection3col.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Image from 'next/image';
+
+function ArticleContainer() {
+  return (
+    <section className='max-w-7xl mx-auto px-6 sm:px-6 lg:px-8 mt-20'>
+      <div className='grid grid-rows-1 md:grid-cols-3 lg:gap-12 gap-6'>
+        <div>
+          <div>
+            <Image
+              src='/louvre2.jpg'
+              alt='Picture of Louvre'
+              width={500}
+              height={400}
+              className='image-shadow'
+            />
+          </div>
+          <div className='flex flex-col md:gap-4 gap-2 pt-4 pb-8'>
+            <p className='font-serif text-gray text-xs uppercase'>travel</p>
+            <h1 className='font-serif text-black text-2xl uppercase'>
+              The Intersection of Art and Travel: Discovering the Worlds Best
+              Museums
+            </h1>
+
+            <p className='font-serif text-black text-xs uppercase'>
+              Lily Alexandra Brown{' '}
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <Image
+              src='/food2.jpg'
+              alt='Picture of a cafe'
+              width={500}
+              height={400}
+              className='image-shadow'
+            />
+          </div>
+          <div className='flex flex-col md:gap-4 gap-2 pt-4 pb-8'>
+            <p className='font-serif text-gray text-xs uppercase'>food</p>
+            <h1 className='font-serif text-black text-2xl uppercase'>
+              Traveling for Foodies: Discovering New Cuisines and Cultures
+            </h1>
+
+            <p className='font-serif text-black text-xs uppercase'>
+              Jonas Andreas Becker{' '}
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <Image
+              src='/taj-mahal.jpg'
+              alt='Picture of a Taj Mahal'
+              width={500}
+              height={400}
+              className='image-shadow'
+            />
+          </div>
+          <div className='flex flex-col md:gap-4 gap-2 pt-4 pb-8'>
+            <p className='font-serif text-gray text-xs uppercase'>
+              ARCHITECTURE
+            </p>
+            <h1 className='font-serif text-black text-2xl uppercase'>
+              10 Must-See Architectural Wonders Around the World
+            </h1>
+
+            <p className='font-serif text-black text-xs uppercase'>
+              BY ETHAN JAMESON
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ArticleContainer;
---
+++ components/ArticleSection4col.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import Image from 'next/image';
+
+function ArticleSection4col() {
+  return (
+    <section className='max-w-7xl mx-auto px-6 sm:px-6 lg:px-8 mt-20'>
+      <h3 className='font-serif text-gray text-lg -mt-10 mb-6'>Latest</h3>
+      <div className='grid grid-rows-1 md:grid-cols-4 lg:gap-8 gap-4'>
+        <div>
+          <div>
+            <Image
+              src='/paris.jpg'
+              alt='Picture of Louvre'
+              width={500}
+              height={400}
+              className='image-shadow'
+            />
+          </div>
+          <div className='flex flex-col md:gap-4 gap-2 pt-4 pb-8'>
+            <p className='font-serif text-gray text-xs uppercase'>Food</p>
+            <h1 className='font-serif text-black text-xl uppercase'>
+              From Street Food to Fine Dining: A Foodies Guide to Paris
+            </h1>
+
+            <p className='font-serif text-black text-xs uppercase'>
+              Leon Matthias Hofmann
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <Image
+              src='/photography2.jpg'
+              alt='Picture of a cafe'
+              width={500}
+              height={400}
+              className='image-shadow'
+            />
+          </div>
+          <div className='flex flex-col md:gap-4 gap-2 pt-4 pb-8'>
+            <p className='font-serif text-gray text-xs uppercase'>
+              photography
+            </p>
+            <h1 className='font-serif text-black text-xl uppercase'>
+              The Art of Travel Photography: Tips for Capturing the World Around
+              You
+            </h1>
+
+            <p className='font-serif text-black text-xs uppercase'>
+              Frieda Johanna Müller
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <Image
+              src='/garden.jpg'
+              alt='Picture of a Taj Mahal'
+              width={500}
+              height={400}
+              className='image-shadow'
+            />
+          </div>
+          <div className='flex flex-col md:gap-4 gap-2 pt-4 pb-8'>
+            <p className='font-serif text-gray text-xs uppercase'>travel</p>
+            <h1 className='font-serif text-black text-xl uppercase'>
+              Exploring the Worlds Most Beautiful Gardens and Parks
+            </h1>
+
+            <p className='font-serif text-black text-xs uppercase'>
+              BY Amelia Grace Lee
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <Image
+              src='/machu.jpg'
+              alt='Picture of a Taj Mahal'
+              width={500}
+              height={400}
+              className='image-shadow'
+            />
+          </div>
+          <div className='flex flex-col md:gap-4 gap-2 pt-4 pb-8'>
+            <p className='font-serif text-gray text-xs uppercase'>
+              Architecture
+            </p>
+            <h1 className='font-serif text-black text-xl uppercase'>
+              Traveling Through Time: Exploring the Architecture of Ancient
+              Civilizations
+            </h1>
+
+            <p className='font-serif text-black text-xs uppercase'>
+              BY Matthew William Davis
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ArticleSection4col;
