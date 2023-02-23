16897d3862d95b05353bb467e9c2a1db4e9b7efb
martinbolsnes <martinbolsnes@icloud.com>
2023-02-23 14:58:45 +0100

    create divider comp

---
+++ components/Divider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Divider() {
+  return (
+    <div className='max-w-7xl mx-auto px-6 sm:px-6 lg:px-8 '>
+      <div className='border-solid border-b-2 border-gray'></div>
+    </div>
+  );
+}
+
+export default Divider;
