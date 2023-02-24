126d0d8177a3d56fc0f8ba6e96c9695b4407d4ee
martinbolsnes <martinbolsnes@icloud.com>
2023-02-24 15:25:27 +0100

    add transistion

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,8 @@
 import { Disclosure, Transition } from '@headlessui/react';
 import { HiOutlineBars3BottomRight, HiXMark } from 'react-icons/hi2';
-
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -12,6 +13,7 @@ const navigation = [
 
 export default function Navigation() {
   const router = useRouter();
+  const [isShowing, setIsShowing] = useState(false);
   return (
     <Disclosure as='nav' className='bg-neutral-50'>
       {({ open }) => (
@@ -32,7 +34,7 @@ export default function Navigation() {
                     {navigation.map((item) => (
                       // eslint-disable-next-line react/jsx-key
 
-                      <a
+                      <Link
                         key={item.name}
                         href={item.href}
                         className={`px-3 py-2 text-base font-sans text-neutral-900 hover:text-neutral-500 ${
@@ -42,12 +44,15 @@ export default function Navigation() {
                         }`}
                       >
                         {item.name}
-                      </a>
+                      </Link>
                     ))}
                   </div>
                 </div>
                 <div className='absolute inset-y-0 right-0 flex items-center sm:hidden'>
-                  <Disclosure.Button className='inline-flex items-center justify-center p-2 text-neutral-900 hover:text-neutral-500 focus:outline-none focus:ring-inset focus:ring-transparent'>
+                  <Disclosure.Button
+                    onClick={() => setIsShowing((isShowing) => !isShowing)}
+                    className='inline-flex items-center justify-center p-2 text-neutral-900 hover:text-neutral-500 focus:outline-none focus:ring-inset focus:ring-transparent'
+                  >
                     <span className='sr-only'>Open main menu</span>
                     {open ? (
                       <HiXMark className='block h-8 w-8' aria-hidden='true' />
@@ -62,25 +67,34 @@ export default function Navigation() {
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className='sm:hidden mt-16'>
-            <div className='px-2 pt-10 pb-3 space-y-1 flex flex-col items-center h-screen'>
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as='a'
-                  href={item.href}
-                  className={`px-3 py-2 block text-base md:text-lg font-sans hover:text-neutral-500 ${
-                    router.pathname === `/${item.href}`
-                      ? 'text-neutral-500'
-                      : ''
-                  }`}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
+          <Transition
+            show={isShowing}
+            enter='transition ease-in-out duration-300 transform'
+            enterFrom='translate-x-full'
+            enterTo='-translate-x-0'
+            leave='transition ease-in-out duration-200 transform'
+            leaveFrom='-translate-x-0'
+            leaveTo='translate-x-full'
+          >
+            <Disclosure.Panel className='sm:hidden mt-16'>
+              <div className='px-2 pt-10 pb-3 space-y-1 flex flex-col items-center h-screen'>
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as='a'
+                    href={item.href}
+                    className={`px-3 py-2 block text-base md:text-lg font-sans hover:text-neutral-500 ${
+                      router.pathname === `/${item.href}`
+                        ? 'text-neutral-500'
+                        : ''
+                    }`}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </Transition>
         </>
       )}
     </Disclosure>
