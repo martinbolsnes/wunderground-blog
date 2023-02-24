d48e99d303bf0c579eac3e92c539e274da871b53
martinbolsnes <martinbolsnes@icloud.com>
2023-02-24 13:44:44 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,42 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite-react/**/*.js',
   ],
   theme: {
-    extend: {
-      colors: {
-        black: '#070606',
-        white: '#FFFDFA',
-        gray: '#6D6D64',
-        green: '#4F7942',
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      neutral: {
+        50: '#fafafa',
+        100: '#f5f5f5',
+        200: '#e5e5e5',
+        300: '#d4d4d4',
+        400: '#a3a3a3',
+        500: '#737373',
+        600: '#525252',
+        700: '#404040',
+        800: '#262626',
+        900: '#171717',
       },
+
+      emerald: {
+        50: '#ecfdf5',
+        100: '#d1fae5',
+        200: '#a7f3d0',
+        300: '#6ee7b7',
+        400: '#34d399',
+        500: '#10b981',
+        600: '#059669',
+        700: '#047857',
+        800: '#065f46',
+        900: '#064e3b',
+      },
+    },
+    extend: {
       fontFamily: {
         serif: ['Lora', 'serif'],
         sans: ['Unbounded', 'cursive'],
@@ -22,5 +46,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
