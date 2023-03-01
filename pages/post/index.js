0c2ebc895e46424fea56fbaf258cfb6bf5d20b94
martinbolsnes <martinbolsnes@icloud.com>
2023-03-01 11:36:18 +0100

    edit title

--- pages/post/index.js
+++ pages/post/index.js
@@ -30,7 +30,7 @@ const Index = ({ posts }) => {
                     <div>
                       <Image
                         src={urlFor(mainImage).url()}
-                        alt=''
+                        alt={`${title}`}
                         width={500}
                         height={400}
                         priority='true'
