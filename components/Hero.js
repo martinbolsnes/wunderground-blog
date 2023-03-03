9a65e6b4bc0ccac1de208f24666297b59d640956
martinbolsnes <martinbolsnes@icloud.com>
2023-03-03 13:15:23 +0100

    fetch data from backend

--- components/Hero.js
+++ components/Hero.js
@@ -12,35 +12,53 @@ function Hero({ posts }) {
   return (
     <section className='max-w-7xl mx-auto px-6 sm:px-6 lg:px-8 mt-32'>
       <div className='flex flex-col lg:flex-row w-full'>
-        <div className='lg:w-1/2 w-full'>
-          <Image
-            src='/heroImg.jpg'
-            alt='Picture of a builing'
-            width={772}
-            height={500}
-            priority='true'
-            className='image-shadow'
-          />
-        </div>
-        <div className='flex flex-col lg:w-1/2 w-full gap-4 mt-4 lg:ml-6'>
-          <p className='font-sans font-light text-neutral-800 text-xs uppercase'>
-            ARCHITECTURE
-          </p>
-          <h1 className='font-sans font-medium text-neutral-900 md:text-4xl text-2xl uppercase'>
-            The Art of Architecture: How to Find Beauty in Buildings
-          </h1>
-          <p className='font-serif text-neutral-700 text-base'>
-            Architecture is often thought of as a practical art form, concerned
-            primarily with function and utility. However, just like other forms
-            of art, architecture can also be beautiful, inspiring, and
-            thought-provoking. The ability to appreciate the aesthetic qualities
-            of buildings is an important part of understanding and enjoying the
-            world around us.
-          </p>
-          <p className='font-sans text-neutral-900 text-xs uppercase'>
-            BY ETHAN JAMESON
-          </p>
-        </div>
+        {posts &&
+          posts.map(
+            ({
+              _id,
+              title = '',
+              slug = '',
+              mainImage = '',
+              categories = '',
+              name = '',
+            }) =>
+              slug && (
+                <div key={_id}>
+                  <Link href={`/post/${encodeURIComponent(slug.current)}`}>
+                    <div className='lg:w-1/2 w-full'>
+                      <Image
+                        src={urlFor(mainImage).url()}
+                        alt={`${title}`}
+                        width={772}
+                        height={500}
+                        priority='true'
+                        className='image-shadow'
+                      />
+                    </div>
+                    <div className='flex flex-col lg:w-1/2 w-full gap-4 mt-4 lg:ml-6'>
+                      <p className='font-sans font-light text-neutral-800 text-xs uppercase'>
+                        {categories}
+                      </p>
+                      <h1 className='font-sans font-medium text-neutral-900 md:text-4xl text-2xl uppercase'>
+                        {title}
+                      </h1>
+                      <p className='font-serif text-neutral-700 text-base'>
+                        Architecture is often thought of as a practical art
+                        form, concerned primarily with function and utility.
+                        However, just like other forms of art, architecture can
+                        also be beautiful, inspiring, and thought-provoking. The
+                        ability to appreciate the aesthetic qualities of
+                        buildings is an important part of understanding and
+                        enjoying the world around us.
+                      </p>
+                      <p className='font-sans text-neutral-900 text-xs uppercase'>
+                        BY {name}
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              )
+          )}
       </div>
     </section>
   );
@@ -49,15 +67,14 @@ function Hero({ posts }) {
 export async function getStaticProps() {
   const posts = await client.fetch(
     groq`
-      *[_type == "post" && publishedAt < now()] | order(publishedAt desc){
-        title,
-          _id,
-          slug,
-          publishedAt,
-        "name": author->name,
-        "categories": categories[]->title,
-        "mainImage": mainImage,
-      }
+    *[_type == "post" && featured]{
+      title,
+        _id,
+        slug,
+      "name": author->name,
+      "categories": categories[]->title,
+      "mainImage": mainImage,
+    }
     `
   );
   return {
